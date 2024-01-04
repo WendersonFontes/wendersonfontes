9a14d84a6dbfefcda4903080db4bb9c56132895d
WendersonFontes <81767673+WendersonFontes@users.noreply.github.com>
2024-01-04 13:04:56 -0300

    Create Create concatenacao_de_palavras.py

---
+++ Create concatenacao_de_palavras.py
@@ -0,0 +1,8 @@
+p1 = input("Digite a primeira palavra: ")
+p2 = input("Digite a segunda palavra: ")
+p3 = input("Digite a terceira palavra: ")
+
+palavras = [p1, p2, p3]
+frase = " ".join(palavras)
+
+print(f"A frase formada é: {frase}")
